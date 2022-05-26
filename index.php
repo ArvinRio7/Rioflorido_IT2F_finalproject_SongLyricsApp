bd584ba110122742547237e3dbea791e52f79ec9
RiofloridoArvin <arioflorido61@gmail.com>
2022-05-26 12:18:19 +0800

    Xml Updated

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ if (isset($_GET['logout'])) {
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="index.php">Home</a>
                     <a class="nav-link fw-bold py-1 px-0" href="lyrics.php">Top Lyrics</a>
-                    <a href="?logout" class="nav-link fw-bold py-1 px-0">Sign out</a>
+                    <a class="nav-link fw-bold py-1 px-0" href="signin.php">Sign in</a>
                 </nav>
             </div>
         </header>
